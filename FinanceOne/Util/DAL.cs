612518166e724b6b139aef03c0f006f34b88a7c1
Guilherme Pompilio <guilherme.pomp@gmail.com>
2018-06-16 23:01:20 -0300

    Conexão MySQL e tela de login nao funcional

---
+++ FinanceOne/Util/DAL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace FinanceOne.Util
+{
+    public class DAL
+    {
+        private static string server = "localhost";
+        private static string database = "financeiro";
+        private static string user = "root";
+        private static string password = "";
+        private string connectionString = $"Server={server};Database={database};Uid={user};Pwd={password};SslMode=none";
+        private MySqlConnection connection;
+
+        public DAL()
+        {
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
+        }
+
+        //Executa Selects
+        public DataTable RetDataTable(string sql)
+        {
+            DataTable dataTable = new DataTable();
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            MySqlDataAdapter da = new MySqlDataAdapter(command);
+            da.Fill(dataTable);
+            return dataTable;
+        }
+
+        //Executa Inserts, Updates e Deletes
+        public void ExecutarComandoSQL(string sql)
+        {
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            command.ExecuteNonQuery();
+        }
+    }
+}
