5892ddd46e2879d1c80e11d475a292d49a7ff8cb
Guilherme Pompilio <guilherme.pomp@gmail.com>
2018-06-21 22:23:51 -0300

    Cadastro de contas bancárias - Part 2

--- FinanceOne/Controllers/ContaController.cs
+++ FinanceOne/Controllers/ContaController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FinanceOne.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FinanceOne.Controllers
 {
     public class ContaController : Controller
     {
+        IHttpContextAccessor HttpContextAccessor;
+
+        public ContaController(IHttpContextAccessor httpContextAccessor)
+        {
+            HttpContextAccessor = httpContextAccessor;
+        }
+
         public IActionResult Index()
         {
-            ContaModel objConta = new ContaModel();
+            ContaModel objConta = new ContaModel(HttpContextAccessor);
             ViewBag.ListaConta = objConta.ListaConta();
             return View();
         }
--- FinanceOne/Models/ContaModel.cs
+++ FinanceOne/Models/ContaModel.cs
@@ -1,4 +1,5 @@
 ﻿using FinanceOne.Util;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -13,13 +14,25 @@ namespace FinanceOne.Models
         public string Nome { get; set; }
         public double Saldo { get; set; }
         public int Usuario_Id { get; set; }
+        IHttpContextAccessor HttpContextAccessor;
+
+        public ContaModel()
+        {
+
+        }
+
+        //Recebe o conceito para acesso das variaveis de sessão.
+        public ContaModel(IHttpContextAccessor httpContextAccessor)
+        {
+            HttpContextAccessor = httpContextAccessor;
+        }
 
         public List<ContaModel> ListaConta()
         {
             List<ContaModel> lista = new List<ContaModel>();
             ContaModel Item;
 
-            string id_usuario_logado = "1";
+            string id_usuario_logado = HttpContextAccessor.HttpContext.Session.GetString("IdUsuarioLogado");
             string sql = $"SELECT ID, NOME, SALDO, USUARIO_ID FROM CONTA WHERE USUARIO_ID={id_usuario_logado}";
             DAL objDAL = new DAL();
             DataTable dt = objDAL.RetDataTable(sql);
